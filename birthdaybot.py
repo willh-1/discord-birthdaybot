8f2cbb467a9ca5bc80e9676ab195fdf7358c4d42
willh-1 <49282565+willh-1@users.noreply.github.com>
2022-04-11 19:38:25 -0700

    testing code time

--- birthdaybot.py
+++ birthdaybot.py
@@ -27,7 +27,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 # loop to get birthday
-@tasks.loop(minutes=1)
+@tasks.loop(minutes=0.5)
 async def change_status(channel):
     today = date.today().strftime("%d.%m")
     # day/month
@@ -41,7 +41,7 @@ async def change_status(channel):
         print("name: ", name)
         if (today == birthday["bday"]):
             message = "@everyone It's " + name + "'s birthday!! \n"
-            if (current_time == "18:24"):
+            if (current_time == "02:39"):
                 await channel.send(message)
     
 
